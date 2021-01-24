f96e87796e4a18bb0b72b6a758a014bf842a6aa0
ismail simsek <ismailsimsek09@gmail.com>
2021-01-25 00:06:42 +0100

    Add home component css module

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import { TweeterLike } from '../components/TweeterLike';
+import styles from './/Home.module.css';
 
 export default function Home() {
   return (
-    <div>
-      <h2>RxJS Tweeter Like App!</h2>
+    <div className={styles.wrapper}>
+      <h1>RxJS Tweeter Like App!</h1>
       <TweeterLike />
     </div>
   );
