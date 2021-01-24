691c4e88fdacee68420dd09e39ad84a55dab5e30
ismail simsek <ismailsimsek09@gmail.com>
2021-01-24 19:06:31 +0100

    Add base tweet stream

---
+++ stream/twit-stream.ts
@@ -0,0 +1,24 @@
+import { interval, merge } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+const createTweetSource = (
+  frequency: number,
+  account: string,
+  attribute: string
+) => {
+  return interval(frequency).pipe(
+    map(i => ({
+      account,
+      timestamp: Date.now(),
+      content: `${attribute} Tweet number ${i + 1}`,
+    }))
+  );
+};
+
+export const tweets = merge(
+  createTweetSource(5000, 'AwardsDarwin', 'Facepalm'),
+  createTweetSource(3000, 'iamdevloper', 'Expert'),
+  createTweetSource(5000, 'CommitStrip', 'Funny')
+);
+
+// tweets.subscribe(console.log.bind(console));
